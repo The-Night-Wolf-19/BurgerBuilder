e569a7465392dac8db0a2083b8439ad9dc345002
Shubham Sinha <shubhamsinha.cer18@itbhu.ac.in>
2021-04-09 20:52:55 +0530

    Added Burger Component

--- burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import Burger from "../../Components/Burger/Burger";
 import Auxiliary from "../../hoc/Auxiliary";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      cheese: 0,
+      bacon: 0,
+      salad: 0,
+      meat: 0,
+    },
+  };
   render() {
     return (
       <Auxiliary>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Auxiliary>
     );
